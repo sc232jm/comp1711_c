70aac761dd8a0ee745e58b6f5d8b9f338f07e7d1
sc232jm <145260982+sc232jm@users.noreply.github.com>
2023-10-12 21:36:12 +0000

    Added string reverse and prime checker

---
+++ week2_c_bootcamp/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+bool checkPrime(int i) {
+    for(int j=2; j<i/2; j++) {
+        if(i%j == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void reverse_string(char *str)
+{
+    int len = strlen(str);
+
+    //Need to instance the array in the heap for the it to be returned globally.
+    char new_str[len];
+
+    char *end = str + len - 1;
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("%p\n", end);
+        printf("%c\n", *end);
+
+        new_str[i] = *end;
+        end--;
+    }
+
+    new_str[len] = '\0';
+
+    printf("Reversed: %s\n", new_str);
+    //return new_str;
+}
+
+int main() {
+    char str[] = "Hello World";
+
+    printf("Original: %s\n", str);
+
+    reverse_string(str);
+
+    //printf("Reversed string: %s\n", reversed_str);
+
+    printf("Input a limit: \n");
+    int limit;
+    scanf("%i", &limit);
+    for(int i=2; i<limit; i++) {
+        bool result = checkPrime(i);
+        printf("%i %s\n", i, result ? "Prime" : "Non-Prime");
+    };
+    return 0;
+}
